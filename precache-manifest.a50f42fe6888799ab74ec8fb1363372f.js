3d3b0dbe5c5166ebd76fd49c6145cc1e5fed6437
Scott Brunswig <sbrunswig@gmail.com>
2020-01-31 07:35:24 -0600

    new react app

---
+++ precache-manifest.a50f42fe6888799ab74ec8fb1363372f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9f75180031c43eb8ba77973bd59c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533c0bd716105037fb4f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2223773457f39f62b281",
+    "url": "/static/js/2.02dbb1cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.02dbb1cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "533c0bd716105037fb4f",
+    "url": "/static/js/main.072806cc.chunk.js"
+  },
+  {
+    "revision": "0b688dc7032f8bb4ca7a",
+    "url": "/static/js/runtime-main.88275c38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
